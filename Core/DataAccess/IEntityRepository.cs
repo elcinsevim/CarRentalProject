44a3bf027b3f5e9c6511b28c41906da3a8b76236
Elçin SEVİM <46248880+elcinsevim@users.noreply.github.com>
2021-02-20 17:57:55 +0300

    20.02.2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace Core.DataAccess
 {
    public interface IEntityRepository<T> where T:class,IEntity,new ()
-   
+   //
     {
         List<T> GetAll(Expression<Func<T, bool>> expressionFilter = null);
 
